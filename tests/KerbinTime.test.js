611ca2c0d846f307acd0b639a9a9ef363e9a0cab
jocooper <jocooper2018@gmail.com>
2025-02-15 16:59:56 +0100

    test: update KerbinTime.test.js: Added KerbinTime.toString() tests.

--- tests/KerbinTime.test.js
+++ tests/KerbinTime.test.js
@@ -70,7 +70,7 @@ test('Value of KerbinTime(1, 0, 0, 0) === 21600', () => {
     expect(kerbinTime.valueOf()).toBe(21600);
 });
 
-test('Value of KerbinTime(2, 0, 0, 0) === 0', () => {
+test('Value of KerbinTime(2, 0, 0, 0) === 43200', () => {
     let kerbinTime = new KerbinTime(2, 0, 0, 0);
     expect(kerbinTime.valueOf()).toBe(43200);
 });
@@ -85,3 +85,112 @@ test('Value of KerbinTime(1, 0, 0, 0, 0) === 9201600', () => {
     expect(kerbinTime.valueOf()).toBe(9201600);
 });
 
+test('toString of KerbinTime()', () => {
+    let kerbinTime = new KerbinTime();
+    expect(kerbinTime.toString()).toBe("0y, 0d, 0h, 0m, 0s");
+});
+
+test('toString of KerbinTime(0)', () => {
+    let kerbinTime = new KerbinTime(0);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 0h, 0m, 0s");
+});
+
+test('toString of KerbinTime(1)', () => {
+    let kerbinTime = new KerbinTime(1);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 0h, 0m, 1s");
+});
+
+test('toString of KerbinTime(30)', () => {
+    let kerbinTime = new KerbinTime(30);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 0h, 0m, 30s");
+});
+
+test('toString of KerbinTime(60)', () => {
+    let kerbinTime = new KerbinTime(60);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 0h, 1m, 0s");
+});
+
+test('toString of KerbinTime(200)', () => {
+    let kerbinTime = new KerbinTime(200);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 0h, 3m, 20s");
+});
+
+test('toString of KerbinTime(1, 0)', () => {
+    let kerbinTime = new KerbinTime(1, 0);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 0h, 1m, 0s");
+});
+
+test('toString of KerbinTime(1, 30)', () => {
+    let kerbinTime = new KerbinTime(1, 30);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 0h, 1m, 30s");
+});
+
+test('toString of KerbinTime(90)', () => {
+    let kerbinTime = new KerbinTime(90);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 0h, 1m, 30s");
+});
+
+test('toString of KerbinTime(2, 0)', () => {
+    let kerbinTime = new KerbinTime(2, 0);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 0h, 2m, 0s");
+});
+
+test('toString of KerbinTime(1, 0, 0)', () => {
+    let kerbinTime = new KerbinTime(1, 0, 0);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 1h, 0m, 0s");
+});
+
+test('toString of KerbinTime(3600)', () => {
+    let kerbinTime = new KerbinTime(3600);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 1h, 0m, 0s");
+});
+
+test('toString of KerbinTime(2, 0, 0)', () => {
+    let kerbinTime = new KerbinTime(2, 0, 0);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 2h, 0m, 0s");
+});
+
+test('toString of KerbinTime(7200)', () => {
+    let kerbinTime = new KerbinTime(7200);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 2h, 0m, 0s");
+});
+
+test('toString of KerbinTime(1, 1, 1)', () => {
+    let kerbinTime = new KerbinTime(1, 1, 1);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 1h, 1m, 1s");
+});
+
+test('toString of KerbinTime(3661)', () => {
+    let kerbinTime = new KerbinTime(3661);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 1h, 1m, 1s");
+});
+
+test('toString of KerbinTime(1, 2, 3)', () => {
+    let kerbinTime = new KerbinTime(1, 2, 3);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 1h, 2m, 3s");
+});
+
+test('toString of KerbinTime(3723)', () => {
+    let kerbinTime = new KerbinTime(3723);
+    expect(kerbinTime.toString()).toBe("0y, 0d, 1h, 2m, 3s");
+});
+
+test('toString of KerbinTime(1, 0, 0, 0)', () => {
+    let kerbinTime = new KerbinTime(1, 0, 0, 0);
+    expect(kerbinTime.toString()).toBe("0y, 1d, 0h, 0m, 0s");
+});
+
+test('toString of KerbinTime(2, 0, 0, 0)', () => {
+    let kerbinTime = new KerbinTime(2, 0, 0, 0);
+    expect(kerbinTime.toString()).toBe("0y, 2d, 0h, 0m, 0s");
+});
+
+test('toString of KerbinTime(2, 3, 4, 5)', () => {
+    let kerbinTime = new KerbinTime(2, 3, 4, 5);
+    expect(kerbinTime.toString()).toBe("0y, 2d, 3h, 4m, 5s");
+});
+
+test('toString of KerbinTime(1, 0, 0, 0, 0)', () => {
+    let kerbinTime = new KerbinTime(1, 0, 0, 0, 0);
+    expect(kerbinTime.toString()).toBe("1y, 0d, 0h, 0m, 0s");
+});

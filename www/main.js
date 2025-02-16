2426df28adb50980fa1982ec4a140e239f19420a
jocooper <jocooper2018@gmail.com>
2025-02-16 13:06:57 +0100

    update www/index.html and main.js Added functionality to input decimal value.

--- www/main.js
+++ www/main.js
@@ -1,4 +1,5 @@
 import KerbinTime from "./modules/KerbinTime.js";
+import { isInt } from "./modules/utils.js";
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -16,13 +17,17 @@ document.addEventListener("DOMContentLoaded", function() {
             timeText.innerText = "-y, -d, -h, -m, -s";
             return;
         }
-        timeText.innerText = new KerbinTime(
-            Number(yearsInput.value), 
-            Number(daysInput.value), 
-            Number(hoursInput.value), 
-            Number(minutesInput.value), 
-            Number(secondsInput.value)
-        ).relayDeploymentPeriod(parseInt(nbSatInput.value)).toString();
+        try {
+            timeText.innerText = new KerbinTime(
+                Number(yearsInput.value), 
+                Number(daysInput.value), 
+                Number(hoursInput.value), 
+                Number(minutesInput.value), 
+                Number(secondsInput.value)
+            ).relayDeploymentPeriod(parseInt(nbSatInput.value)).toString();
+        } catch (e) {
+            timeText.innerText = "-y, -d, -h, -m, -s";
+        }
     }
 
     function canBeDowned(input) {
@@ -42,37 +47,47 @@ document.addEventListener("DOMContentLoaded", function() {
         return false;
     }
 
-    function handleInputChange(input1, input2, max) {
-        if (input1.value >= max) {
-            input2.value = Number(input2.value) + Math.floor(input1.value / max);
-            input1.value = input1.value % max;
-        } else if (input1.value < 0) {
-            if (canBeDowned(input2)) {
-                input2.value -= Math.ceil(Math.abs(input1.value) / max);
-                input1.value = max - Math.abs(input1.value) % max;
+    function handleInputChange(input, inputUp, inputDown, max, maxDown) {
+        if ((inputDown !== null) && !isInt(input.value)) {
+            inputDown.value = Number(inputDown.value) + ((Number(input.value) - parseInt(input.value)) * maxDown);
+            input.value = parseInt(input.value);
+            inputDown.dispatchEvent(new Event("change"));
+        }
+        if (input.value >= max) {
+            inputUp.value = Number(inputUp.value) + Math.floor(input.value / max);
+            input.value = input.value % max;
+            inputUp.dispatchEvent(new Event("change"));
+        } else if (input.value < 0) {
+            if ((inputUp !== null) && canBeDowned(inputUp)) {
+                inputUp.value -= Math.ceil(Math.abs(input.value) / max);
+                input.value = max - Math.abs(input.value) % max;
+                inputUp.dispatchEvent(new Event("change"));
             } else {
-                input1.value = 0
+                input.value = 0;
             }
         }
-        input2.dispatchEvent(new Event("change"));
         updateTimeText();
     }
 
     secondsInput.addEventListener("change", function() {
-        handleInputChange(secondsInput, minutesInput, KerbinTime.MAX_SECONDS);
+        handleInputChange(secondsInput, minutesInput, null, KerbinTime.MAX_SECONDS, null);
     });
 
     minutesInput.addEventListener("change", function() {
-        handleInputChange(minutesInput, hoursInput, KerbinTime.MAX_MINUTES);
+        handleInputChange(minutesInput, hoursInput, secondsInput, KerbinTime.MAX_MINUTES, KerbinTime.MAX_SECONDS);
     });
 
     hoursInput.addEventListener("change", function() {
-        handleInputChange(hoursInput, daysInput, KerbinTime.MAX_HOURS);
+        handleInputChange(hoursInput, daysInput, minutesInput, KerbinTime.MAX_HOURS, KerbinTime.MAX_MINUTES);
     });
 
     daysInput.addEventListener("change", function() {
-        handleInputChange(daysInput, yearsInput, KerbinTime.MAX_DAYS);
+        handleInputChange(daysInput, yearsInput, hoursInput, KerbinTime.MAX_DAYS, KerbinTime.MAX_HOURS);
     });
+
+    yearsInput.addEventListener("change", function() {
+        handleInputChange(yearsInput, null, daysInput, Infinity, KerbinTime.MAX_DAYS);
+    })
 
     nbSatInput.addEventListener("change", function() {
         updateTimeText();
